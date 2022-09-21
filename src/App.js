ba320e885d1c345496179b773e6ba661e94a7861
caiohbe <caiohbe@gmail.com>
2022-09-21 20:20:12 -0300

    playable

--- src/App.js
+++ src/App.js
@@ -1,50 +1,65 @@
 import React from "react"
 import palavras from "./palavras"
 
-function randomizer() { 
-	return Math.random() - 0.5; 
-}
-
-const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-const alphabet = alfabeto.map((letter) => {
-    return (
-        <letter className="selectable" onClick={() => attempt(letter)}>
-            {letter.toUpperCase()}
-        </letter>
-    )
-})
-
+let progress = []
 let answer
 let answerArr = []
 let erros = 0
 
-function attempt(letter) {
-    console.log(letter)
-    console.log(answerArr)
-}
-
 export default function App() {
     const [word, setWord] = React.useState('')
 
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const alphabet = alfabeto.map((letter) => {
+        return (
+            <letter className="selectable" onClick={() => attempt(letter)}>
+                {letter.toUpperCase()}
+            </letter>
+        )
+    })
+
+    function attempt(letter) {
+        for (let i = 0; i < answerArr.length; i++) {
+            if (answerArr[i].normalize("NFD").replace(/[\u0300-\u036f]/g, "") === letter) {
+                console.log('SIM')
+                progress[i] = answerArr[i]
+            }
+        }
+
+        let word = ''
+        progress.forEach((e) => {
+            word += `${e} `
+        })
+
+        setWord(word)
+        console.log(word)
+        console.log(answerArr)
+        console.log(progress)
+    }
+
+
     function selectWord() {
+        answerArr = []
+        progress = []
         let underline = ''
-        palavras.sort(randomizer)
+
+        palavras.sort(() => Math.random() - 0.5)
         answer = palavras[0]
+
         for (let i = 0; i < palavras[0].length; i++) {
             answerArr.push(palavras[0][i])
             underline += '_ '
+            progress.push('_')
         }
-        console.log(answer)
-        console.log(answerArr)
-        
         setWord(underline)
+        console.log(answer)
     }
 
 
     return (
-        <content>
+        <React.Fragment>
             <upper>
-                <img src="assets/forca0.png" alt="forca"/>
+                <img src="assets/forca0.png" alt="forca" />
                 <right>
                     <button className="select" onClick={selectWord}>Escolher Palavra</button>
                     <p>{word}</p>
@@ -58,7 +73,6 @@ export default function App() {
                 <input type="text"></input>
                 <button className="try">Chutar</button>
             </attempt>
-            
-        </content>
+        </React.Fragment>
     )
 }
